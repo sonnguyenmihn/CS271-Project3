c0a13fc69c52a3371d1143936c3c30095d8014b0
Esther Zhang <zhang_e1@denison.edu>
2024-12-01 19:00:55 -0500

    Update Set.hpp
    Having some testing issues gonna go to lab to test in a sec

--- Set.hpp
+++ Set.hpp
@@ -4,3 +4,34 @@
 // Nov, 2024
 // This is the header file with definitions of functions for the Set class.
 //=========================================================
+#ifndef Set_hpp
+#define Set_hpp
+
+#include <iostream>
+#include "RBTree.hpp"
+#include "RBTreeNode.hpp"
+#include "Set.hpp"
+
+using namespace std;
+
+template <class T>
+class Set {
+private:
+    RBTree<int> RBT;
+    
+public:
+    //Constructors and Destructor
+    set();
+    set(const Set& s);
+    ~set();
+
+    //Functions
+    void insert(const T &value);
+    void remove(const T &value);
+    bool search(const T &value);
+
+    //Helper Functions
+    void printSet();
+};
+
+#endif
