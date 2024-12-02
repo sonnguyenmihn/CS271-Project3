7d7dbf6d20410327b3be4ad3f2f2e6c4a4407949
Esther Zhang <zhang_e1@denison.edu>
2024-12-01 19:03:38 -0500

    Update mytests.cpp
    To be tested

--- mytests.cpp
+++ mytests.cpp
@@ -31,7 +31,13 @@ void test_HashMap_remove(int &testPassed, int &testFailed);
 void test_HashMap_operator_access(int &testPassed, int &testFailed);
 void test_HashMap_search(int &testPassed, int &testFailed);
 
-
+//Set
+void test_Set_default_constructor(int &testPassed, int &testFailed);
+void test_Set_copy_constructor(int &testPassed, int &testFailed);
+void test_Set_deconstructor(int &testPassed, int &testFailed);
+void test_Set_insert(int &testPassed, int &testFailed);
+void test_Set_remove(int &testPassed, int &testFailed);
+void test_Set_search(int &testPassed, int &testFailed);
 
 int main() {
     int testPassed = 0;
@@ -54,7 +60,15 @@ int main() {
     test_HashMap_remove(testPassed, testFailed);
     test_HashMap_operator_access(testPassed, testFailed);
     test_HashMap_search(testPassed, testFailed);
-
+    
+    // Set tests
+    test_Set_default_constructor(testPassed, testFailed);
+    test_Set_copy_constructor(testPassed, testFailed);
+    test_Set_deconstructor(testPassed, testFailed);
+    test_Set_insert(testPassed, testFailed);
+    test_Set_remove(testPassed, testFailed);
+    test_Set_search(testPassed, testFailed);
+    
     cout << "Total Tests: " << testPassed + testFailed << endl;
     cout << "Tests Passed: " << testPassed << endl;
     cout << "Tests Failed: " << testFailed << endl;
@@ -408,5 +422,166 @@ void test_HashMap_search(int &testPassed, int &testFailed) {
     } catch (const exception &e) {
         testFailed++;
         cout << "Failed test_HashMap_search: Exception caught" << endl;
+    }
+}
+
+
+//=========================================================
+// Test Default Constructor Set
+// Purpose:
+//   - Verify that an empty set is created
+//=========================================================
+void test_Set_default_constructor(int &testPassed, int &testFailed) {
+    try {
+        Set<int> set; // Empty set
+        if (set.search(root) == false) {
+            testPassed++;
+            cout << "Passed test_Set_default_constructor" << endl;
+        } else {
+            testFailed++;
+            cout << "Failed test_Set_default_constructor" << endl;
+        }
+    } catch (const exception &e) {
+        testFailed++;
+        cout << "Failed test_Set_default_constructor: Exception caught" << endl;
+    }
+}
+
+//=========================================================
+// Test Copy Constructor Set
+// Purpose:
+//   - Verify that a set is created uing a prexisting set
+//=========================================================
+void test_Set_copy_constructor(int &testPassed, int &testFailed) {
+    try {
+        Set<int> set;
+        set.insert(5);
+        set.insert(7);
+        set.insert(1);
+
+        copySet = set(set);
+
+        if (copySet.search(root) == true) {
+            testPassed++;
+            cout << "Passed test_Set_copy_constructor" << endl;
+        } else {
+            testFailed++;
+            cout << "Failed test_Set_copy_constructor" << endl;
+        }
+    } catch (const exception &e) {
+        testFailed++;
+        cout << "Failed test_Set_copy_constructor: Exception caught" << endl;
+    }
+}
+
+//=========================================================
+// Test Deconstructor Set
+// Purpose:
+//   - Verify that a set is properly deconstructed
+//=========================================================
+void test_Set_deconstructor(int &testPassed, int &testFailed) {
+    try {
+        Set<int> set;
+        set.insert(5);
+        set.insert(7);
+        set.insert(1);
+
+        ~set();
+
+        if (set.search(root) == false) {
+            testPassed++;
+            cout << "Passed test_Set_deconstructor" << endl;
+        } else {
+            testFailed++;
+            cout << "Failed test_Set_deconstructor" << endl;
+        }
+    } catch (const exception &e) {
+        testFailed++;
+        cout << "Failed test_Set_deconstructor: Exception caught" << endl;
+    }
+}
+
+//=========================================================
+// Test Insert Set
+// Purpose:
+//   - Verify that values are properly inserted into the Set
+//=========================================================
+void test_Set_insert(int &testPassed, int &testFailed) {
+    try {
+        Set<int> set;
+        set.insert(5);
+        set.insert(7);
+        set.insert(3);
+        set.insert(1);
+        set.insert(4);
+
+
+        if (set.search(5) == true && set.search(7) == true && set.search(1) == true
+        && set.search(3) == true && set.search(4) == true) {
+            testPassed++;
+            cout << "Passed test_Set_insert" << endl;
+        } else {
+            testFailed++;
+            cout << "Failed test_Set_insert" << endl;
+        }
+    } catch (const exception &e) {
+        testFailed++;
+        cout << "Failed test_Set_insert: Exception caught" << endl;
+    }
+}
+
+//=========================================================
+// Test Remove Set
+// Purpose:
+//   - Verify that values are properly removed from the Set
+//=========================================================
+void test_Set_remove(int &testPassed, int &testFailed) {
+try {
+        Set<int> set;
+        set.insert(5);
+        set.insert(7);
+        set.insert(3);
+        set.insert(1);
+        set.insert(4);
+
+        set.remove(4);
+        set.remobe(3);
+
+        if (set.search(4) == false && set.search(3) == false) {
+            testPassed++;
+            cout << "Passed test_Set_remove" << endl;
+        } else {
+            testFailed++;
+            cout << "Failed test_Set_remove" << endl;
+        }
+    } catch (const exception &e) {
+        testFailed++;
+        cout << "Failed test_Set_remove: Exception caught" << endl;
+    }
+}
+
+//=========================================================
+// Test Search Set
+// Purpose:
+//   - Verifies that search correctly states whether a value is in a set
+//=========================================================
+void test_Set_search(int &testPassed, int &testFailed) {
+    try {
+        Set<int> set;
+        set.insert(5);
+        set.insert(7);
+        set.insert(1);
+
+        if (set.search(5) == true && set.search(7) == true && set.search(1) == true
+        && set.search(2) == false && set.search(9) == false) {
+            testPassed++;
+            cout << "Passed test_Set_search" << endl;
+        } else {
+            testFailed++;
+            cout << "Failed test_Set_search" << endl;
+        }
+    } catch (const exception &e) {
+        testFailed++;
+        cout << "Failed test_Set_search: Exception caught" << endl;
     }
 }
